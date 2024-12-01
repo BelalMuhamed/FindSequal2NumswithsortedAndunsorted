0694d91eeaff530117e8a3cf000b3edc46af71a8
BelalBasal <basdibrahim@gmail.com>
2024-12-01 19:57:24 +0200

    unsortedarrayv01

--- FindSequal2Nums/FindSequal2Nums.cpp
+++ FindSequal2Nums/FindSequal2Nums.cpp
@@ -1,9 +1,40 @@
 
 
 #include <iostream>
-
+using namespace std;
+bool FindTwoNumbersEqualNumInUnsortedArray(int Numbers[], int size, int SearchValue)
+{
+    bool flag = false;
+    for (int i = 0;i < size;i++)
+    {
+        for (int j = 0;j < size;j++)
+        {
+            if (i == j)
+                continue;
+            else 
+            {
+                if (SearchValue == (Numbers[i] + Numbers[j]))
+                {
+                    flag = true;
+                    break;
+                }
+                    
+                
+                
+                    
+            }
+            
+        }
+        if (flag == true)
+        {
+            break;
+        }
+    }
+    return flag;
+}
 int main()
 {
-    stdcout << "Hello World!\n";
+    int numbers[10] = { 1,5,8,9,7,12,3,99,15,17 };
+    cout << FindTwoNumbersEqualNumInUnsortedArray(numbers, 10, 200);
 }
 
